213ef20b19023ceab06816e8550094b3fc23c332
Terence Lee <hone02@gmail.com>
2015-06-12 18:10:06 -0700

    now takes a PATCH with a body

--- commands/enable.js
+++ commands/enable.js
@@ -16,7 +16,18 @@ Example:
     needsAuth: true,
     needsApp: true,
     run: function (context) {
-      request.post(`${baseUrl}/apps/${context.app}/cdn`, onResponse).auth('', context.auth.password, true);
+      request.patch({
+        url: `${baseUrl}/apps/${context.app}/cdn`,
+        json: true,
+        body: {
+          'enabled': true
+        },
+        auth: {
+          username: '',
+          password: context.auth.password,
+          sendImmediately: true
+        }
+      }, onResponse);
       
       function onResponse(err, res, body) {
         if (err) {
@@ -25,13 +36,7 @@ Example:
         }
 
         if(res.statusCode == 200) {
-          try {
-            var parsedBody = JSON.parse(body)
-          }
-          catch(e) {
-            throw e;
-          }
-          cli.log(`CDN is being provisioned. Run \`heroku ${topic}:wait\` to check the status. Set your DNS CNAME to '${parsedBody.hostname}'`);
+          cli.log(`CDN is being provisioned. Run \`heroku ${topic}:wait\` to check the status. Set your DNS CNAME to '${body.hostname}'`);
         } else {
           cli.error(`Problem provisioning your CDN. ${res.statusCode}`);
         }
